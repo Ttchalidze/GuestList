0a1e631acc5705b1378d8770ea6c404d2a54fcbe
tornike thcalidze <ttchalidze@gmail.com>
2025-03-30 23:59:20 -0400

    first1

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,34 @@
 export default function App() {
-  return <></>;
+  const { data: recipes, loading, error } = useQuery("/recipes");
+  if (loading) return <p>Loading</p>;
+  if (error || !recipes) return <p>{error}</p>;
+
+  return (
+    <>
+      <h1>Recipes</h1>
+      <RecipeColletion recipes={recipes} />
+    </>
+  );
+}
+function RecipeColletion({ recipes }) {
+  return (
+    <article className="recipes">
+      {recipes.map((recipe) => (
+        <RecipeCard key={recipe.id} recipe={recipe} />
+      ))}
+    </article>
+  );
+}
+function RecipeCard({ recipe }) {
+  return (
+    <article className="recipe">
+      <h2>
+        {recipe.name}#{recipe.id}
+      </h2>
+      <figure>
+        <img alt={recipe.name} src={recipe.imgUrl} />
+      </figure>
+      <p>{recipe.description}</p>
+    </article>
+  );
 }
