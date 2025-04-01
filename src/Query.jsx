4244a13405b5f73e3007b9b319e808b1ffe63ec1
tornike thcalidze <ttchalidze@gmail.com>
2025-03-31 23:47:18 -0400

    first1

--- src/Query.jsx
+++ src/Query.jsx
@@ -3,14 +3,13 @@ import { useState, useEffect } from "react";
 const BASE_URL = "https://fsa-crud-2aa9294fe819.herokuapp.com/api";
 const COHORT = "/TK";
 const API = BASE_URL + COHORT;
+
 export default function useQuery(resource) {
   const [data, setData] = useState();
-  const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     const query = async () => {
-      setLoading(true);
       setError(null);
       try {
         const response = await fetch(API + resource);
@@ -20,12 +19,10 @@ export default function useQuery(resource) {
       } catch (e) {
         console.error(e);
         setError(e.message);
-      } finally {
-        setLoading(false);
       }
     };
     query();
   }, [resource]);
 
-  return { data, loading, error };
+  return { data, error };
 }
