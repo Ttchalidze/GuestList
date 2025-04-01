612c2315bff55c454a9e7062c6a6e666f7254a7e
tornike thcalidze <ttchalidze@gmail.com>
2025-03-31 23:46:53 -0400

    first1

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,16 @@
-export default function App() {
-  const { data: recipes, loading, error } = useQuery("/recipes");
-  if (loading) return <p>Loading</p>;
-  if (error || !recipes) return <p>{error}</p>;
+import { useState } from "react";
+import GuestDetails from "./guestdetails";
+import GuestList from "./guestlist";
 
+export default function App() {
+  const [guestID, setGuestID] = useState(null);
   return (
-    <>
-      <h1>Recipes</h1>
-      <RecipeColletion recipes={recipes} />
-    </>
-  );
-}
-function RecipeColletion({ recipes }) {
-  return (
-    <article className="recipes">
-      {recipes.map((recipe) => (
-        <RecipeCard key={recipe.id} recipe={recipe} />
-      ))}
-    </article>
-  );
-}
-function RecipeCard({ recipe }) {
-  return (
-    <article className="recipe">
-      <h2>
-        {recipe.name}#{recipe.id}
-      </h2>
-      <figure>
-        <img alt={recipe.name} src={recipe.imgUrl} />
-      </figure>
-      <p>{recipe.description}</p>
-    </article>
+    <main>
+      {guestID ? (
+        <GuestDetails guestID={guestID} setGuestID={setGuestID} />
+      ) : (
+        <GuestList setGuestID={setGuestID} />
+      )}
+    </main>
   );
 }
